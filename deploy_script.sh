8d0d1c4fa12ce3e0c649c31f2d0401d5b495aa52
Giacomo Del Vecchio <giacomo.delvecchio@salesforce.com>
2023-11-24 11:23:33 +0100

    modified script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Function to log messages
 log() {
   local message="$1"
@@ -49,8 +48,9 @@ check_anypoint_cli_version() {
 replace_group_id() {
   local directory="$1"
   local org_id="$2"
-  log "Replacing GROUP_ID in $directory/pom.xml..."
-  sed -i "s/GROUP_ID/$org_id/g" "$directory/pom.xml"
+  local file="$3"
+  log "Replacing GROUP_ID in $directory/$file..."
+  sed -i '' -e "s/GROUP_ID/$org_id/g" "$directory/$file" || exit 1
 }
 
 # Function to replace GROUP_ID placeholder in api.raml and exchange.json
@@ -58,8 +58,8 @@ replace_group_id_additional() {
   local directory="$1"
   local org_id="$2"
   log "Replacing GROUP_ID in $directory/api.raml and $directory/exchange.json..."
-  sed -i "s/GROUP_ID/$org_id/g" "$directory/api.raml"
-  sed -i "s/GROUP_ID/$org_id/g" "$directory/exchange.json"
+  sed -i "s/GROUP_ID/$org_id/g" "$directory/api.raml" || exit 1
+  sed -i "s/GROUP_ID/$org_id/g" "$directory/exchange.json" || exit 1
 }
 
 # Function to rename directories within exchange_modules
@@ -87,9 +87,18 @@ process_directory() {
   log "Processing $directory..."
   cd "$directory" || exit 1
 
-  # Create a local git branch
-  log "Creating local git branch customer/$customer_name..."
-  git checkout -b "customer/$customer_name" || exit 1
+  # Check if the branch already exists
+  if [[ "$skip_git" == true ]]; then
+    if ! git rev-parse --verify "customer/$customer_name" >/dev/null 2>&1; then
+      log "Branch customer/$customer_name does not exist. Creating it..."
+      git checkout -b "customer/$customer_name" || exit 1
+    else
+      log "Branch customer/$customer_name already exists. Using it..."
+      git checkout "customer/$customer_name" || exit 1
+    fi
+  else
+    log "skipping git managment"
+  fi
 
   # Deploy using Maven
   log "Deploying using Maven..."
@@ -103,75 +112,135 @@ process_directory() {
 deploy_with_maven() {
   local directory="$1"
   local control_plane="$2"
-  if [[ "$control_plane" == "eu" ]]; then
-    $mvn_cmd clean deploy
+
+  if [ -n "$tag_version" ]; then
+      log "using a new tag for deployment: $tag_version..."
+      $mvn_cmd -U versions:set -DnewVersion=$tag_version || exit 1
+  fi
+
+  if [[ "$control_plane" == true ]]; then
+    log "running on us control plane..."
+    $mvn_cmd -U --settings $script_dir/.mvn/settings.xml clean deploy -Pcp_us -DskipTests || exit 1
   else
-    $mvn_cmd clean deploy -Pcp_us
+    log "running on eu control plane..."
+    $mvn_cmd -U --settings $script_dir/.mvn/settings.xml clean deploy  -DskipTests || exit 1
   fi
 }
 
 # Function to read JSON file and process directories
 process_directories_from_json() {
-  local json_file="$1"
+  local csv_file="$1"
   log "Reading directories from $json_file..."
 
-  while IFS= read -r line; do
-    local name=$(echo "$line" | jq -r '.name')
-    local type=$(echo "$line" | jq -r '.type')
-
+  while IFS=, read -r name type; do
+    log "Processing directory $name of type: $type..."
     case "$type" in
       "maven")
-        replace_group_id "$name" "$organization_id"
+        replace_group_id "$name" "$organization_id" pom.xml
         process_directory "$name" "$customer_name" "$control_plane"
         ;;
       "raml-api")
-        replace_group_id_additional "$name" "$organization_id"
-        rename_exchange_modules "$name" "$organization_id"
-        process_directory "$name" "$customer_name" "$control_plane"
-        anypoint-cli-v4 designcenter:project:create --client_id $client_application_id --client_secret $client_application_secret --organization $organization_id  --type raml "$name" 
+        if [[ "$skip_design_center" == true ]]; then
+          replace_group_id_additional "$name" "$organization_id"
+          rename_exchange_modules "$name" "$organization_id"
+          process_directory "$name" "$customer_name" "$control_plane"
+          anypoint-cli-v4 designcenter:project:create --client_id $client_application_id --client_secret $client_application_secret --organization $organization_id  --type raml "$name" 
+          else
+            log "skipping design center project..."
+        fi
         ;;
       "raml-fragment")
-        replace_group_id_additional "$name" "$organization_id"
-        rename_exchange_modules "$name" "$organization_id"
-        process_directory "$name" "$customer_name" "$control_plane"
-        anypoint-cli-v4 designcenter:project:create --client_id $client_application_id --client_secret $client_application_secret --organization $organization_id --type raml_fragment "$name" 
-        ;;
+        if [[ "$skip_design_center" == true ]]; then
+          replace_group_id_additional "$name" "$organization_id"
+          rename_exchange_modules "$name" "$organization_id"
+          process_directory "$name" "$customer_name" "$control_plane"
+          anypoint-cli-v4 designcenter:project:create --client_id $client_application_id --client_secret $client_application_secret --organization $organization_id --type raml_fragment "$name" 
+        else
+            log "skipping design center project..."
+        fi
+         ;;
       *)
         log "Error: Unsupported directory type."
         exit 1
         ;;
     esac
-  done < <(jq -c '.directories[]' "$json_file")
+  done < "$csv_file"
 
   log "Finished processing directories."
 }
+# Function to display script usage
+show_usage() {
+  echo "Usage: $0 [-t tag_version] [-w] [-g] [-d] [-c] customer_name organization_id connected_app_id connected_app_secret"
+  exit 1
+}
+
 
 # Main script starts here
-control_plane=$1
-customer_name=$2
-organization_id=$3
-client_application_id=$4
-client_application_secret=$5
+log "Start deploy."
 
-#check if jq is available in the environment
-which jq
-if [[ $? != 0 ]]; then
-  log "jq utility is not installed."
-  exit 1
+
+# Process optional flags
+while getopts ":t:w:g:d:c" opt; do
+  case $opt in
+    t)
+      tag_version=$OPTARG
+      ;;
+    w)
+      use_wrapper=true
+      ;;
+    g)
+      skip_git=true
+      ;;
+    d)
+      control_plane=true
+      ;;
+    c)
+      skip_design_center=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      show_usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      show_usage
+      ;;
+  esac
+done
+
+# Shift the processed options out of the positional parameters
+shift $((OPTIND-1))
+
+# Check for the required positional parameters
+if [ "$#" -ne 4 ]; then
+  show_usage
 fi
 
-# Read Java and Maven versions from JSON file
 
-java_version=$(jq -r '.java_version' "deploy_config.json")
-maven_version=$(jq -r '.maven_version' "deploy_config.json")
-maven_wrapper=$(jq -r '.maven_wrapper' "deploy_config.json")
-anypoint_cli_version=$(jq -r '.anypoint_cli_version' "deploy_config.json")
+customer_name=$1
+organization_id=$2
+client_application_id=$3
+client_application_secret=$4
+script_dir=$(pwd)
 
-if [[ "$maven_wrapper" != "true" ]]; then
-   $mvn_cmd="./mvnw"
+#export variables
+export MULE_CONNECTED_APP_CLIENT_ID=$client_application_id
+export MULE_CONNECTED_APP_CLIENT_SECRET=$client_application_secret
+
+# versions
+java_version="1.8"
+maven_version="3.9.5"
+anypoint_cli_version="1.0.0"
+
+if [[ "$use_wrapper" == true ]]; then
+   log "Use maven wrapper."
+   mvn_cmd="$script_dir/mvnw"
   else
-   $mvn_cmd="mvn"
-  fi
+   log "Use Environment Maven."
+   mvn_cmd="mvn"
+fi
+
+replace_group_id ".mvn" "$organization_id" settings.xml
 
 # Check Java and Maven versions
 check_anypoint_cli_version
@@ -180,4 +249,4 @@ check_maven_version
 
 
 # Process directories from JSON file
-process_directories_from_json "deploy_config.json"
+process_directories_from_json "deploy_config.csv"
